7d36f926caffe64ca3b810e9f95324e5fad15424
Brent Besselievre <brent@Brents-MacBook-Air.local>
2022-01-11 16:21:34 -0700

    Initial form for creating new user

--- redsky-challenge-frontend-js/src/App.js
+++ redsky-challenge-frontend-js/src/App.js
@@ -2,13 +2,21 @@ import React, { useEffect, useState }  from 'react';
 import axios from 'axios';
 
 import UserList from './components/UserList';
+import NewUserForm from './components/NewUserForm';
 
+
+const initialCreateUserValues = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  avatar: ''
+}
 const initialUsers = []
-
 
 function App() {
 
   const [ users, setUsers ] = useState(initialUsers);
+  const [ createUserFormValues, setCreateUserFormValues ] = useState(initialCreateUserValues)
 
   const getUsers = () => {
     axios.get('https://reqres.in/api/users')
@@ -29,6 +37,7 @@ function App() {
     <div className="flow">
       <h1 className='ff-serif fs-600 uppercase'>Redsky Coding Challenge</h1>
       <button>Create New User</button>
+      <NewUserForm createUserFormValues={createUserFormValues} setCreateUserFormValues={setCreateUserFormValues} />
       <UserList users={users}/>
     </div>
   );
--- redsky-challenge-frontend-js/src/components/NewUserForm.js
+++ redsky-challenge-frontend-js/src/components/NewUserForm.js
@@ -1,9 +1,56 @@
 import React from 'react';
 
-const NewUserForm = () => {
+const NewUserForm = (props) => {
+
+    const { createUserFormValues, setCreateUserFormValues } = props;
+
+    const changeHandler = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        setCreateUserFormValues({...createUserFormValues, [name]: value})
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+    }
     return(
         <div>
-            <h1>Create New User</h1>
+            <h3>Create New User</h3>
+            <form>
+                <label>First Name</label>
+                <input 
+                type='text'
+                name='first_name'
+                value={createUserFormValues.first_name}
+                onChange={changeHandler}
+                />
+
+                <label>Last Name</label>
+                <input 
+                type='text'
+                name='last_name'
+                value={createUserFormValues.last_name}
+                onChange={changeHandler}
+                />
+
+                <label>Email Address</label>
+                <input 
+                type='email'
+                name='email'
+                value={createUserFormValues.email}
+                onChange={changeHandler}
+                />
+
+                <label>Avatar Image Link</label>
+                <input 
+                type='text'
+                name='avatar'
+                value={createUserFormValues.avatar}
+                onChange={changeHandler}
+                />
+
+                <button onClick={submitHandler}>Submit</button>
+            </form>
         </div>
     )
 }
